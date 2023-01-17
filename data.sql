0ee4180b26938f063b73a5cef2ebda1b6eb0ca1b
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-01-17 17:05:58 +0300

    add screenshots

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered ) VALUES (1,'Agumon', '02/03/2020',0, 10.23, true);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered ) VALUES (2,'Gabumon', '10/15/2022', 2, 15.04, false);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered ) VALUES (3,'Pikachu', '01/13/2020', 1, 0.5, false);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered ) VALUES (4,'Devimon', '05/04/2017', 5, 11.13, true);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT *
+FROM  animals
+WHERE name LIKE '%mon'
+SELECT name FROM animals where date_of_birth BETWEEN '01/01/2016' AND  '01/01/2019';
+SELECT name FROM animals where neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals where name ='Agumon' or name= 'Pikachu' ;
+SELECT name, escape_attempts FROM animals where weight_kg > 10.5;
+SELECT * FROM animals where neutered = true;
+SELECT * FROM animals where name NOT LIKE 'Gabumon';
+SELECT * FROM animals where weight_kg BETWEEN 10.4 and 17.3;
